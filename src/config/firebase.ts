d1c434819573ea56fcd029d91223dcc36feb4bb9
Cristian Quintero Sánchez <cristian.quintero@pragma.com.co>
2023-11-06 18:13:45 -0500

    add firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,26 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  MEASUREMENT_ID,
+  MESSAGING_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from '@env';
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export default app;
