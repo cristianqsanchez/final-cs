d2734388f324468a5957fd636750faa446fea364
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-11-10 13:44:46 -0500

    Update add tweet feature

--- src/components/TweetComponent.tsx
+++ src/components/TweetComponent.tsx
@@ -5,16 +5,33 @@ import {View, Text} from 'react-native';
 import {useForm} from 'react-hook-form';
 import {arrayUnion, doc, getDoc, updateDoc} from 'firebase/firestore';
 import {db} from '../config/firebase';
-
+function createTweet(author, username, content) {
+  return {
+    author: author,
+    username: username,
+    content: content,
+    date: Date.now(),
+  };
+}
 function TweetComponent({id}) {
   const {control, handleSubmit} = useForm();
   const saveTweet = async data => {
     try {
       const userRef = doc(db, 'users', id);
-      const content = data.content;
-      await updateDoc(userRef, {
-        tweets: arrayUnion(content),
-      });
+      const userDoc = await getDoc(userRef);
+
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+
+        console.log(userData.email);
+       
+
+        await updateDoc(userRef, {
+          tweets: arrayUnion(
+            createTweet(userData.fullname, userData.username, data.content),
+          ),
+        });
+      }
     } catch (error) {
       console.error('Error submitting tweet:', error);
     }
