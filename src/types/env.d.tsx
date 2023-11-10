c47a7867d0f58507c811b12cb47ba54245f2e9de
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-11-09 22:39:31 -0500

    Update

--- src/types/env.d.tsx
+++ src/types/env.d.tsx
@@ -1,9 +1,9 @@
 declare module '@env' {
-  export const API_KEY: 'AIzaSyBUriNiCISL2tjwAB_65yOUpJezDwE1oo4';
-  export const AUTH_DOMAIN: 'twitter-f9e75.firebaseapp.com';
-  export const PROJECT_ID: 'twitter-f9e75';
-  export const STORAGE_BUCKET: 'twitter-f9e75.appspot.com';
-  export const MESSAGING_SENDER_ID: '1016657856326';
-  export const APP_ID: '1:1016657856326:web:4b468f0234c17df79c0747';
-  export const MEASUREMENT_ID: 'G-TBL4DWFV3B';
+  export const API_KEY: string;
+  export const AUTH_DOMAIN: string;
+  export const PROJECT_ID: string;
+  export const STORAGE_BUCKET: string;
+  export const MESSAGING_SENDER_ID: string;
+  export const APP_ID: string;
+  export const MEASUREMENT_ID: string;
 }
