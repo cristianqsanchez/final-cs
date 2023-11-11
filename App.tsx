90957030cd21ac2deb721e305844cc948c48aa7e
Cristian Quintero Sánchez <cristian.quintero@pragma.com.co>
2023-11-11 11:41:04 -0500

    add navigation to tweet responses

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/views/home';
 import Login from './src/views/login';
+import TweetDetail from './src/views/tweet-detail';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ function App(): JSX.Element {
       <Stack.Navigator initialRouteName="login">
         <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="tweet-detail" component={TweetDetail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
